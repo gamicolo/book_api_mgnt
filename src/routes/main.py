b3da3d5027a0392439bb5e869961da7fe3fa6b03
gamicolo <guillermoamicolo@gmail.com>
2023-01-26 15:28:51 -0300

    feat:add CRUD operations to book comments

--- src/routes/main.py
+++ src/routes/main.py
@@ -60,3 +60,46 @@ def store_book_info(book_isbn):
         return jsonify("Done"),200
 
     return "",200
+
+@main.route('/api/v1/assessment/book_comments_management/<string:book_isbn>', methods=['GET','PUT','DELETE','OPTIONS'])
+def book_comments_management(book_isbn):
+
+    code = 404
+    reply = "The book doesn't exist on the database"
+    
+    book_stored = Book.query.filter(Book.isbn == book_isbn).first()
+
+    if not(book_stored):
+        return jsonify(reply),code
+
+    if request.method == 'PUT':
+        book_stored.comments = request.json['book_comments']
+        try:
+            db.session.commit()
+            code = 200
+            reply = 'Success update of the comment in the book'
+        except:
+            reply = 'ERROR trying to update the comment of the book on the DB'
+            print(reply)
+
+        return jsonify(reply),code
+
+    if request.method == 'GET':
+        code = 200
+        reply = 'Success getting the comment of the book'
+
+        return jsonify(book_stored.comments),code
+
+    if request.method == 'DELETE':
+        book_stored.comments = '' 
+        try:
+            db.session.commit()
+            code = 200
+            reply = 'Success deleting the comment of the book'
+        except:
+            reply = 'ERROR trying to delete the comment of the book'
+            print(reply)
+
+        return jsonify(reply),code
+
+    return "",200
