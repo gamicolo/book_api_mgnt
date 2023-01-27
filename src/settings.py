f0f685253ea4bef4fe3a6fd94b83d89ae9f1269c
gamicolo <guillermoamicolo@gmail.com>
2023-01-27 11:25:16 -0300

    feat:define database uri to be used from another path (not locally)

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,9 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+DB_NAME = os.environ.get('SQLALCHEMY_DATABASE_NAME') or 'db.sqlite3'
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, DB_NAME)}"
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
